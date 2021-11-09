c7d3983bad436055b118f482c35c336241ccbd07
EmsCode82 <emsgithub@gmail.com>
2021-11-09 14:53:37 -0600

    .

---
+++ src/package1/Hello.java
@@ -0,0 +1,39 @@
+package package1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		/*
+		int y = 65000;
+		short x = (short)y;
+		System.out.println(x);
+		
+
+		double v2 = 20.0123456789d;
+		float v1 = (float)v2;
+		System.out.println(v1);
+		*/
+		
+		/*
+		float x = 10.0f;
+		int y = (int)x;
+		System.out.println(y);
+		*/
+		
+		/*
+		int x = 10;
+		float y = x;
+		System.out.println(y);
+		*/
+		
+		
+		
+		float f = 10.532f;
+		long l = (long) f;
+		System.out.println(l);
+		
+		
+		
+	}
+
+}
